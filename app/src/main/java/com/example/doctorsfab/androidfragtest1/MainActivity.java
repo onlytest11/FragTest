506f21d869e0634ac289b71878b3386123dba67a
qwertyu5258 <qwertyu5258@naver.com>
2018-07-19 16:03:06 +0900

    3

--- app/src/main/java/com/example/doctorsfab/androidfragtest1/MainActivity.java
+++ app/src/main/java/com/example/doctorsfab/androidfragtest1/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +15,16 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     Button button2;
     Button button3;
-
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        textView1 = findViewById(R.id.TextVew_TestFragment);
         FragmentSetting();
         ButtonSetting();
+
 
     }
 
--- app/src/main/java/com/example/doctorsfab/androidfragtest1/TestFragment.java
+++ app/src/main/java/com/example/doctorsfab/androidfragtest1/TestFragment.java
@@ -4,44 +4,32 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TestFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TestFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+
     private OnFragmentInteractionListener mListener;
+    private TextView textView;
+
+    private static final String tag = "tag >>>>>>>>>>>>>>>> ";
+
 
     public TestFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static TestFragment newInstance(String param1, String param2) {
         TestFragment fragment = new TestFragment();
         Bundle args = new Bundle();
@@ -58,13 +46,23 @@ public class TestFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+
+        Log.d(tag, mParam1);
+        Log.d(tag, mParam2);
+
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+
+        textView = view.findViewById(R.id.TextVew_TestFragment);
+        textView.append("mParam1\n" + mParam1 + "mParam2" + mParam2);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -72,6 +70,8 @@ public class TestFragment extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+
+
     }
 
     @Override
